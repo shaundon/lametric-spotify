f0d4b9856b3cd44a05bfcf41d622f0d77f80faef
Shaun Donnelly <shaundonnelly@gmail.com>
2018-10-28 18:23:33 +0000

    More stuff

--- README.md
+++ README.md
@@ -2,7 +2,26 @@
 
 ## About
 
-Back end for a [LaMetric Time]() to poll and get now playing info for your Spotify account.
+Back end for a [LaMetric Time](https://lametric.com/) to poll and get now playing info for your Spotify account.
+
+## Rationale and use case
+
+I use an Amazon Echo in my kitchen to listen to music a lot. I also have a LaMetric Time in there. I thought it would be cool to display the currently playing song on it. I hoped that this would already exist, but unfortunately it doesn't (there is an IFTTT trigger for Amazon Echo 'now playing' changing, but it currently only works with Prime Music).
+
+So, this is my hack to get this functionality.
+
+### Specific speakers
+
+As this is linked directly to Spotify and not my Amazon Echo, it will display my now playing info even if I'm out and about. Some people might find this desirable but I don't want this functionality, as I want it to be local to the home. 
+
+So, there's extra logic to have it only return a result if the speaker that Spotify is playing from matches a predefined list. To do this, set the `ALLOWED_DEVICES` environment variable. It's a comma-separated list, so you can specify multiple
+speakers or groups, e.g. `ALLOWED_DEVICES=Kitchen,Downstairs,Living Room`.
+
+## Prerequisites
+
+* Spotify client ID and client secret. Get this from their developer website.
+* LaMetric developer account. You'll need to publish this as a private app and install it onto your device.
+* Somewhere to deploy this back end - e.g. Heroku.
 
 ## Install
 
@@ -13,9 +32,11 @@ npm install
 ## Run
 
 ```
-SPOTIFY_API_TOKEN='put your OAuth token here' npm start
+CLIENT_ID='client id' CLIENT_SECRET='client secret' npm start
 ```
 
-## Tests
+You'll need to send a Spotify authorisation key linked to your account with your requests. To do this, go to `/auth` and you'll be given a URL you can go to to authenticate. After authenticating, the callback URL will have a code as a GET param. Take this code and then send it as a GET param to `/` in your requests, for example `http://localhost:3000?auth_token_0=your_code_here.
 
-Lol nah
+## Future
+
+I want to make it support multiple API keys, so that if one account isn't listening to music I can check another. This is so that multiple people living in the same household can use this app.
--- index.js
+++ index.js
@@ -1,42 +1,73 @@
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
-const spotify = new SpotifyWebApi();
 
-const apiToken = process.env.SPOTIFY_API_TOKEN;
-if (apiToken) {
-    spotify.setAccessToken(process.env.SPOTIFY_API_TOKEN);
-} else {
-    console.error('SPOTIFY_API_TOKEN is not set. Please set this as an env var.');
+const { 
+    CLIENT_ID: clientId, 
+    CLIENT_SECRET: clientSecret 
+} = process.env;
+
+if (!clientId || !clientSecret) {
+    console.error(`Set CLIENT_ID and CLIENT_SECRET as env vars. Exiting..`);
+    process.exit(1);
 }
 
+const spotify = new SpotifyWebApi({
+    clientId,
+    clientSecret,
+    redirectUri: 'http://www.example.com/callback'
+});
+
+let allowedDevices = process.env.ALLOWED_DEVICES;
+
+if (allowedDevices) {
+    allowedDevices = allowedDevices.split(',');
+    console.log(`Setting allowed devices to ${allowedDevices.join(',')}`);
+}
+
+app.get('/auth', (req, res) => {
+    const scopes = ['user-read-playback-state'];
+    const state = 'eba';
+    const authUrl = spotify.createAuthorizeURL(scopes, state);
+    res.send(`It's dangerous to go alone! Take this: ${authUrl}`);
+});
+
 app.get('/', async (req, res) => {
-    const apiToken = process.env.SPOTIFY_API_TOKEN;
-    if (!apiToken) {
-        res.status(500).send('SPOTIFY_API_TOKEN not set.');
+    const { auth_code_0 } = req.query;
+    if (!auth_code_0) {
+        res.status(400).send(`Missing "auth_code_0" GET param.`);
         return;
+    }
+    
+    try {
+        const { access_token } = await spotify.authorizationCodeGrant(auth_code_0);
+        spotify.setAccessToken(process.env.SPOTIFY_API_TOKEN);
+    } catch (err) {
+        res.status(500).send(`Error getting access token`, err);
     }
 
     try {
         const { body: nowPlaying } = await spotify.getMyCurrentPlaybackState();
-        if (nowPlaying['is_playing']) {
-
-            if (nowPlaying.item && nowPlaying.item.name && nowPlaying.item.artists) {
-                const track = nowPlaying.item.name;
-                const artists = nowPlaying.item.artists.map(artist => artist.name).join(', ');
-                res.send({
-                    "frames": [
-                        {
-                            "text": `${track} - ${artists}`,
-                            "icon": "i647"
-                        }
-                    ]
-                });
+        let responseText = '-';
+        if (nowPlaying['is_playing'] && nowPlaying.item && nowPlaying.item.name && nowPlaying.item.artists) {
+            const track = nowPlaying.item.name;
+            const artists = nowPlaying.item.artists.map(artist => artist.name).join(', ');
+            responseText = `${track} - ${artists}`;
+            if (allowedDevices && !allowedDevices.includes(nowPlaying.device.name)) {
+                responseText = '-';
             }
         }
+        res.send({
+            "frames": [
+                {
+                    "text": responseText,
+                    "icon": "i647" // Spotify logo.
+                }
+            ]
+        });
     } catch (err) {
         console.error(err);
-        res.sendStatus(500);
+        res.status(500).send(err);
     }
 })
 
